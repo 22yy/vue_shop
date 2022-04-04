cb97b66661abeb200fbb05bf0609d22168748842
j-p <2061351540@qq.com>
2022-04-04 17:56:48 +0800

    完成用户列表功能开发

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<!--  -->
+<template>
+  <div>Welcome</div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style  scoped>
+</style>
